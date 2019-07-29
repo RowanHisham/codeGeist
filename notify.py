2f5a642c179eb37c5bd83eb8f47501802c8265d8
Rowan Hisham <rowan.hisham133@gmail.com>
2019-07-29 19:34:57 +0200

    email

--- notify.py
+++ notify.py
@@ -5,25 +5,27 @@ from UserClass import User
 from sendgrid.helpers.mail import *
 # # from ExportToExcel import ExcelConverter
 class emailNotifier():
-    def __init__(self, sender, user):
+    def __init__(self, user):
         """
         Constructor to the emailNotifier class, takes sender and recipient email addresses
         """
         self.user = user
-        self.sender = Email(sender)
-        # self.recipient = Email(user.getEmail())
-        self.recipient = Email("abdullahsalah96@outlook.com")
-        self.sg = sendgrid.SendGridAPIClient(apikey = "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")
-        self.content = Content("text/plain", "html_content")
+        self.sender = Email("noreply@pisces.com")
+        self.recipient = Email(user.getEmail())
+        self.sg = sendgrid.SendGridAPIClient(apikey = "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")
+        self.content = Content("text/plain", "EMAIL")
 
     def sendEmail(self):
         """
         a function that sends an email to the user with his fish farm data
         """
-        subject = "subject"
+        subject = "Hello"
         self.mail = Mail(self.sender, subject, self.recipient, self.content)
         response = self.sg.client.mail.send.post(request_body = self.mail.get())
+        print(response.status_code)
+        print(response.body)
+        # print(response.headers)
 
 u = User("ab", "b", "abdullahsalah96", "fdsa", "abdullahsalah96@outlook.com", None, None, None, None)
-e = emailNotifier(sender= "yo@gmail.com", user = u)
+e = emailNotifier(user = u)
 e.sendEmail()
