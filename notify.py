3fa9a6b0da4dd6a9288cb5de51471bd07adf37f1
abdullahsalah96 <abdullahsalah96@outlook.com>
2019-07-29 19:01:43 +0200

    sengrid

--- notify.py
+++ notify.py
@@ -14,26 +14,16 @@ class emailNotifier():
         # self.recipient = Email(user.getEmail())
         self.recipient = Email("abdullahsalah96@outlook.com")
         self.sg = sendgrid.SendGridAPIClient(apikey = "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")
-        self.content = Content("text/html", "html_content")
+        self.content = Content("text/plain", "html_content")
 
     def sendEmail(self):
         """
         a function that sends an email to the user with his fish farm data
         """
-        from_email = Email(self.sender)
         subject = "subject"
-        to_email = Email(self.recipient)
-        content = Content("text/html", "email_body")
+        self.mail = Mail(self.sender, subject, self.recipient, self.content)
+        response = self.sg.client.mail.send.post(request_body = self.mail.get())
 
-        mail = Mail(from_email, subject, to_email, content)
-
-        response = self.sg.client.mail.send.post(request_body=mail.get())
-
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-#
-#
 u = User("ab", "b", "abdullahsalah96", "fdsa", "abdullahsalah96@outlook.com", None, None, None, None)
 e = emailNotifier(sender= "yo@gmail.com", user = u)
 e.sendEmail()
