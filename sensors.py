406fb22b7aeee128eb816d0a1635c179ef5d86a2
Rowan Hisham <rowan.hisham133@gmail.com>
2019-07-29 20:04:41 +0200

    sensors reading

--- sensors.py
+++ sensors.py
@@ -4,7 +4,7 @@ class Sensors():
     def __init__(self, url):
         self.url = url
 
-    def scrapeUrl(url):
+    def scrapeUrl(self, url):
         """
         a function that returns a string of the
         scraped website
@@ -12,12 +12,15 @@ class Sensors():
         r = requests.get(url)
         return(r.text)
 
-    def getTemperature():
+    def getTemperature(self):
         """
         a function that returns the temperature
         reading
         """
-        text = str(scrapeUrl(self.url))
-        tempIndex = text.find("temperature") + 2
-        temperature = text[tempIndex: tempIndex+2]
+        text = str(self.scrapeUrl(self.url))
+        tempIndex = text.find("temperature") + 14
+        temperature = text[tempIndex: tempIndex+5]
         return temperature
+
+s = Sensors("http://192.168.43.223/")
+print(s.getTemperature())
