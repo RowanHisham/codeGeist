547574fd5e7e95175ebbec8a1c4d9e34cfb12673
abdullahsalah96 <abdullahsalah96@outlook.com>
2019-07-29 14:59:32 +0200

    added net holes/cleaning models

--- NetCleaningInspection.py
+++ NetCleaningInspection.py
@@ -1,19 +1,34 @@
 from cognitiveServices import Server
 
+"""
+Getting index of tag is not working
+"""
 
 class NetCleaning(Server):
     def __init__(self):
         #change subAddress
         self.httpAddress = 'australiaeast.api.cognitive.microsoft.com'
-        self.subAddress = "/customvision/v3.0/Prediction/0894325a-97a6-4390-9277-3ecda4230665/classify/iterations/Cars%20or%20Trucks/image"
+        self.subAddress = "/customvision/v3.0/Prediction/8803f1fd-5690-4ed9-8a95-6097586fcf46/classify/iterations/Iteration1/image"
         self.predKey = 'bd5db334ffa249f1bd8ca2d43ccf5da3'
         self.contentType = 'application/octet-stream'
-        self.predkey = '/subscriptions/16e8c7d2-4877-4c8f-bb77-ae2931dd33eb/resourceGroups/Test/providers/Microsoft.CognitiveServices/accounts/Test_Prediction'
+        self.predkey = '/subscriptions/16e8c7d2-4877-4c8f-bb77-ae2931dd33eb/resourceGroups/Test/providers/Microsoft.CognitiveServices/accounts/Test'
         super().__init__(self.httpAddress, self.subAddress, self.predKey, self.contentType, self.predkey)
 
-    def predict(self, imgPath):
+    def predict(self, img):
         """
         a function that returns:
             string: yes, no, can't determine
         """
+        prediction = str(self.getPrediction(img))
+        tag_index = str(prediction).find("tagId") + 8
+        print(tag_index)
+        if(prediction[tag_index:tag_index+1] == '3'):
+            return "no"
+        elif(prediction[tag_index:tag_index+1] == '1'):
+            return "yes"
+        else:
+            return "can't determine"
 
+
+n = NetCleaning()
+print(n.predict("/home/abdullahsalah96/Downloads/me.jpg"))
--- NetHoleInspection.py
+++ NetHoleInspection.py
@@ -1,10 +1,13 @@
 from cognitiveServices import Server
 
+"""
+Getting index of tag is not working
+"""
 
 class NetHoleDetection(Server):
     def __init__(self):
         self.httpAddress = 'australiaeast.api.cognitive.microsoft.com'
-        self.subAddress = "/customvision/v3.0/Prediction/0081ddad-4c13-4705-ac6b-819068f863f8/classify/iterations/Fishnet_Holes_Detection/image"
+        self.subAddress = "/customvision/v3.0/Prediction/1a9e8456-5cd6-4b76-8861-f47b6f4a7f2a/classify/iterations/Iteration1/image"
         self.predKey = 'bd5db334ffa249f1bd8ca2d43ccf5da3'
         self.contentType = 'application/octet-stream'
         self.predkey = '/subscriptions/16e8c7d2-4877-4c8f-bb77-ae2931dd33eb/resourceGroups/Test/providers/Microsoft.CognitiveServices/accounts/Test_Prediction'
@@ -15,3 +18,16 @@ class NetHoleDetection(Server):
         a function that returns:
             string: yes, no, can't determine
         """
+        prediction = str(self.getPrediction(img))
+        tag_index = str(prediction).find("tagId") + 8
+        if(prediction[tag_index:tag_index+1] == '7'):
+            return "no"
+        elif(prediction[tag_index:tag_index+1] == '0'):
+            return "yes"
+        else:
+            return "can't determine"
+
+
+
+n = NetHoleDetection()
+print(n.predict("/home/abdullahsalah96/Pictures/scene00211.png"))
